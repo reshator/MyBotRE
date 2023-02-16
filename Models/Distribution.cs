42f20b806dab7902550e61696fa43e88086b9bab
Resh <Resh@DESKTOP-N8STILE>
2023-02-16 22:23:47 +0500

    add models

---
+++ Models/Distribution.cs
@@ -0,0 +1,13 @@
+﻿namespace MyBotRE.Models
+{
+    internal class Distribution
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? BasedOn { get; set; }
+        public List<string>? DesktopEnvironments { get; set; }
+        public List<string>? Categories { get; set; }
+        public string? Status { get; set; }
+        public string? Description { get; set; }
+    }
+}
